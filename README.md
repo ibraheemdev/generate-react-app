2b3ac8a408f2e0d8e960d1cd6429ac7e1a5f50cb
afamily <ibrah1440@gmail.com>
2020-04-14 13:20:59 -0400

    update readme/blog

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Generated with create-react-app
-Added tailwind css
+
+Setup Tailwind CSS with PostCSS and autoprefixer
+
 Setup PurgeCSS in production
+
+Uses Redux as state management solution, and redux-saga middleware for asynchronous requests.
+
+
--- src/views/blog/blog.js
+++ src/views/blog/blog.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const Home = () => {
+const Blog = () => {
   return (
     <div className="mx-auto my-2 text-center">
-      <div className="text-3xl text-blue-500 font-bold my-4">Your Blog</div>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-2">
-        Stuff Goes Here
+      <div className="text-3xl text-blue-500 font-bold my-4">Blog</div>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-2">
+        Generated with create-react-app <br/> Setup Tailwind CSS with PostCSS and
+        autoprefixer <br/> Setup PurgeCSS in production <br/> Uses Redux as state management
+        solution, and redux-saga middleware for asynchronous requests.
       </button>
     </div>
   );
 };
 
-export default Home;
+export default Blog;
